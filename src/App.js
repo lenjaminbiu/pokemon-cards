b115bf9ff65cb9c8bda5e302e6b67ec81c203221
Benjamin <lenjaminbiu@gmail.com>
2020-10-04 18:57:29 +1000

    fetch api and destructuring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import LandingPage from "./components/landingpage.componet";
+import "./App.css";
 
 function App() {
+  const [currentPokemon, setPokemon] = useState([]);
+  const pokemonRef = useRef();
+
+  async function getPokemon(e) {
+    const searchName = pokemonRef.current.value;
+    e.preventDefault();
+    const allData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${searchName}`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        const {
+          sprites: { front_default },
+        } = data;
+        console.log(front_default);
+      });
+    // if (searchName) {
+    //   // setPokemon({
+    //   //   info: allData,
+    //   //   id: allData.game_indices.game_index,
+    //   //   name: allData.species.name,
+    //   // });
+    //   const {
+    //     game_indices: [{ height, id }],
+    //   } = allData;
+    // }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokedex</h1>
+      <input ref={pokemonRef} type="text" placeholder="Search for a Pokemon" />
+      <button onClick={getPokemon}>Search</button>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/components/landingpage.componet.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = (props) => {
+  return (
+    <div>
+      <h2>Preview Pokemons</h2>
+      <h3>Today's Pokemon's are...</h3>
+    </div>
+  );
+};
+
+export default LandingPage;

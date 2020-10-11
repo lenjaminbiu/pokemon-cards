ec7f815fc354b4a3d1f727bbbc4ecab067bb9f1f
Benjamin <lenjaminbiu@gmail.com>
2020-10-11 15:17:42 +1000

    styling

--- src/App.js
+++ src/App.js
@@ -27,24 +27,22 @@ function App() {
           image: null,
           name: "",
           id: ""
-        })
-
-        }
+        })}
+        pokemonRef.current.value = ''
       });
   }
 
   return (
-    <div>
+    <div className="component">
       <h1>Pokemon Finder</h1>
       <ErrorBoundary>
       <input ref={pokemonRef} type="text" placeholder="Search for a Pokemon" />
-      
         <button onClick={getPokemon}>Search</button>
       <div className='pokemon-card'>
         <h2>Your Pokemon: </h2>
         <img src={currentPokemon.image} alt="" width={200} height={200}/>
-        <h3>Name: {currentPokemon.name}</h3>
-        <h3>Pokedex Number: {currentPokemon.id}</h3>
+        <h3 className="label">Name: {currentPokemon.name}</h3>
+        <h3 className="label">Pokedex Number: {currentPokemon.id}</h3>
       </div>
       </ErrorBoundary>
     </div>
